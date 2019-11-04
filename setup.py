ab1bba864e8ce0ad568db0b24a32e14fb3ec75af
Marc van Dijk <marcvdijk@gmail.com>
2019-11-04 11:59:38 +0100

    Update setup
    Change lie_amber to mdstudio_amber, update version, description and keywords

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Part of ‘lie_amber’, a package providing an interface to the AmberTools
+# package: mdstudio_amber
+# file: setup.py
+#
+# Part of ‘mdstudio_amber’, a package providing an interface to the AmberTools
 # software suite http://ambermd.org.
 #
-# Copyright © 2018 Marc van Dijk, VU University Amsterdam, the Netherlands
+# Copyright © 2019 Marc van Dijk, VU University Amsterdam, the Netherlands
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,25 +23,27 @@
 
 from setuptools import (setup, find_packages)
 
-distribution_name = 'lie_amber'
+distribution_name = 'mdstudio_amber'
 
 setup(
     name=distribution_name,
-    version='0.2.0',
-    description='MDStudio component provinding an interface to the Amber Tools software suite',
+    version=0.2,
+    description='MDStudio component providing an interface to the AmberTools software suite',
     author="""
     Marc van Dijk - VU University - Amsterdam
     Paul Visscher - Zefiros Software (www.zefiros.eu)
     Felipe Zapata - eScience Center (https://www.esciencecenter.nl/)""",
     author_email=['m4.van.dijk@vu.nl', 'f.zapata@esciencecenter.nl'],
-    url='https://github.com/MD-Studio/lie_amber',
+    url='https://github.com/MD-Studio/MDStudio_amber',
     license='Apache Software License 2.0',
-    keywords='MDStudio AmberTools Amber',
+    keywords='MDStudio AmberTools Amber ACPYPE',
     platforms=['Any'],
     packages=find_packages(),
     py_modules=[distribution_name],
     include_package_data=True,
     zip_safe=True,
+    scripts=['scripts/acpype.py'],
+    extras_require={'test': ['coverage']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
@@ -45,11 +51,5 @@ setup(
         'Topic :: Utilities',
         'Operating System :: OS Independent',
         'Intended Audience :: Science/Research',
-        ],
-    scripts=[
-        'scripts/acpype.py'
-    ],
-    extras_require={
-        'test': ['coverage']
-    }
+    ]
 )
